219f2c2b44578de81c0f6f1472a0ab73708aa25d
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-02 16:19:53 +0700

    Clean up minified version

--- src/TeaJS/teajs/minified/ajax.js
+++ src/TeaJS/teajs/minified/ajax.js
@@ -2,5 +2,5 @@ teajs.ajax=function(data){let ch=new XMLHttpRequest;if(typeof data.onload==="fun
 if(typeof data.method!=="string"){data.method="GET"}
 if(typeof data.url!=="string"){data.url=""}
 if((typeof data.withoutCredential==="undefined")||data.withoutCredential){ch.withCredentials=!0}
-ch.overrideMimeType('text/plain; charset=iso-8859-1');ch.open(data.method,data.url);ch.setRequestHeader("X-Requested-With","XMLHttpRequest");if(typeof data.header==="object"){let x;for(x in data.header){ch.setRequestHeader(x,data.header[x])}}
+ch.open(data.method,data.url);ch.setRequestHeader("X-Requested-With","XMLHttpRequest");if(typeof data.header==="object"){let x;for(x in data.header){ch.setRequestHeader(x,data.header[x])}}
 ch.send(data.data)}
